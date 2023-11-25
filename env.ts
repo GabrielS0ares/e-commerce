5208aa25bca76279df61c0e24affb561240ba6cc
GabrielS0ares <soaresgabriel33@gmail.com>
2023-11-25 13:22:08 -0300

    Feat: Criando Fetch API wrapper

---
+++ env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+const parseEnv = envSchema.safeParse(process.env)
+
+if (!parseEnv.success) {
+  console.error(
+    'Variaveis do ENV inválidas',
+    parseEnv.error.flatten().fieldErrors,
+  )
+
+  throw new Error('Variaveis do ENV inválidas')
+}
+
+export const env = parseEnv.data
