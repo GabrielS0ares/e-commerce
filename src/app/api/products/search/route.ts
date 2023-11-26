d3f6fb92d44fd306e176ca399656bb77d35d9414
GabrielS0ares <soaresgabriel33@gmail.com>
2023-11-26 13:56:26 -0300

    Feat: Buscando produtos da API

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -5,10 +5,11 @@ import { z } from 'zod'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl
+
   const query = z.string().parse(searchParams.get('q'))
 
   const products = data.products.filter((product) => {
-    return product.title.toLocaleUpperCase().includes(query.toLocaleLowerCase())
+    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
   })
 
   return Response.json(products)
